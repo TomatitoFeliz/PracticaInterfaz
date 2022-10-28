61862d0bf10caf1c37c26704d3c23001f147d4e3
TomatitoFeliz <66531110+TomatitoFeliz@users.noreply.github.com>
2022-10-28 13:33:43 +0100

    actualizacion 0.11

--- PracticaInterfaz/Assets/Scripts/ScriptsMenu/Changelenguage.cs
+++ PracticaInterfaz/Assets/Scripts/ScriptsMenu/Changelenguage.cs
@@ -6,23 +6,7 @@ using UnityEngine.Localization.Settings;
 public class Changelenguage : MonoBehaviour
 {
     int idioma = 0;
-    //void Awake()
-    //{
-        //Array copn los diferentes idiomas que tiene el juego configurado
-        //LocalizationSettings.AvailableLocales.Locales
-        //Idiomas = (Español, Inglés, Aleman, Francés, Italiano,...);
-
-        //El idioma que se está utilizando
-        //LocalizationSettings.SelectedLocale
-
-        //int idiomaAleatorio = Random.Range(0, 4); //Genero número aleatorio
-        //LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[idiomaAleatorio];
-        //¿Le da límite al array? ¿Me dice los idiomas que hay?
-        //int langAvailables = LocalizationSettings.AvailableLocales.Locales.Count;
-    //}
-
-    //Función Next Language
-    public void Next()
+   public void Next()
     {
         Debug.Log("Click on Next");
         idioma++;
@@ -47,5 +31,43 @@ public class Changelenguage : MonoBehaviour
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[idioma];
     }
 
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+    //private void Start()
+    //{
+        //SelectCurrentLang();
+    //}
+    //void SelectCurrentLang()
+    //{
+    //UnityEngine.Localization.Locale searcher = LocalizationSettings.AvailableLocales.Locales[idioma];
+
+    //Mientras el idioma que está funcionando no es el que estoy revisando, miro el siguiente.
+    //while (searcher != LocalizationSettings.SelectedLocale && idioma < LocalizationSettings.AvailableLocales.Locales.Count)
+        //{
+            //idioma++;
+            //searcher = LocalizationSettings.AvailableLocales.Locales[idioma];
+        //}
+
+    //}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+    //void Awake()
+    //{
+    //Array copn los diferentes idiomas que tiene el juego configurado
+    //LocalizationSettings.AvailableLocales.Locales
+    //Idiomas = (Español, Inglés, Aleman, Francés, Italiano,...);
+
+    //El idioma que se está utilizando
+    //LocalizationSettings.SelectedLocale
+
+    //int idiomaAleatorio = Random.Range(0, 4); //Genero número aleatorio
+    //LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[idiomaAleatorio];
+    //¿Le da límite al array? ¿Me dice los idiomas que hay?
+    //int langAvailables = LocalizationSettings.AvailableLocales.Locales.Count;
+    //}
+
+    //Función Next Language
+ 
 
 }
